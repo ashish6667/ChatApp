77b4729990fd8a96e200e7debb01d31f2bcca78c
ashish6667 <ay1060045@gmail.com>
2025-04-09 17:30:25 +0530

    Fix syntax error by adding a missing comma in token expiration settings

--- Backend/jwt/genrateToken.js
+++ Backend/jwt/genrateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 const createTokenAndSaveCookie=(userId,res)=>{
     const token = jwt.sign({ userId }, process.env.JWT_TOKEN,{
-        expiresIn: "10d"
+        expiresIn: "10d",
     });
     res.cookie("jwt",token,{
         httpOnly: true, // xss
